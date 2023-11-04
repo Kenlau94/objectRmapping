65bbe381f9986dd9b8678671356adc8f212d6a64
Kenlau94 <kenlauwork94@gmail.com>
2023-11-04 15:57:51 -0400

    addedCatCode

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // todo define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
