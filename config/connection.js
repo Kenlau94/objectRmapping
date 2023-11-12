46b0920fa876c34a72f59643ac6002f89f10e198
Kenlau94 <kenlauwork94@gmail.com>
2023-11-11 22:07:46 -0500

    trying to fix bugs, no run

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,17 @@ require("dotenv").config();
 
 const Sequelize = require("sequelize");
 
+//chat gtp said noooooooooo, ruining the code haha
+// const Sequelize = new Sequelize(
+//   process.env.DB_DATABASE,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//   }
+// );
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
